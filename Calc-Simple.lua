afa8aa888609c6f092bb60945775c94d50ac4b7a
Mike Elliot <jedersonninho@gmail.com>
2026-09-01 21:41:47 -0300

    Add files via upload

---
+++ Calc-Simple.lua
@@ -0,0 +1,25 @@
+lg()
+
+ui.color("blue", "bold")
+ui.fig("Calculadora")
+ui.color("reset")
+VariavelNumeroUmPrimeiroNumero = tonumber(input("Primeiro Number: "))
+VariavelNumeroDoisSegundoNumero = tonumber(input("Segundo Number: "))
+VariavwlNumeroTresSimboloArimatic = input("Simbolo Arimatico: ")
+
+if !3 == "+" then
+    print(!1 + !2)
+elseif !3 == "-" then
+    print(!1 - !2)
+elseif !3 == "/" then
+    print(!1 / !2)
+elseif !3 == "*" then
+print(!1 * !2)
+elseif !3 == "//" then
+print(!1 // !2)
+elseif !3 == "%" then
+print(!1 % !2)
+elseif !3 == "^" then
+print(!1 ^ !2)
+else print("Nao reconhecido")
+end
